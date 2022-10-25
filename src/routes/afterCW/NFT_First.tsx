a04e943b10863c3f5f2e24f7e1ea846f10643fa9
kyulee <kyulee1338@gmail.com>
2022-10-26 08:40:09 +0900

    fix: emergency

--- src/routes/afterCW/NFT_First.tsx
+++ src/routes/afterCW/NFT_First.tsx
@@ -235,7 +235,7 @@ function NFT_First() {
 
     if (!isMinted) {
       // alert(`checkMintResult ${JSON.stringify(checkMintResult)} address ${address}`);
-      navigate('/final', {
+      navigate('/whoyou', {
         state: {
           myAddress: address,
         },
