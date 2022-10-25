7420360cb6adb96bcfaad59512cbb124b1afb384
kyulee <kyulee1338@gmail.com>
2022-10-26 02:58:58 +0900

    fix : 찐찐막

--- src/routes/Hello.tsx
+++ src/routes/Hello.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import Lottie from 'lottie-react';
@@ -8,6 +8,7 @@ import FileSaver from 'file-saver';
 import { blob } from 'stream/consumers';
 import { saveAs } from 'file-saver';
 import moving from '../assets/move.gif';
+import domtoimage from 'dom-to-image';
 
 interface IHeight {
   height: number;
@@ -24,46 +25,61 @@ const LogoDIv = styled.div`
 `;
 
 function Hello() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      navigate('/Main');
-    }, 6000);
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const timeoutId = setTimeout(() => {
+  //     navigate('/Main');
+  //   }, 6000);
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   };
+  // }, []);
 
   const newUrl = new Blob(['https://nftmetadata2022.s3.ap-northeast-2.amazonaws.com/images/1.png']);
   console.log(newUrl);
 
-  const downloadNFT = () => {
-    const url = 'https://nftmetadata2022.s3.ap-northeast-2.amazonaws.com/images/1.png';
-    fetch(url, {
-      mode: 'no-cors',
-      method: 'GET',
-      headers: {
-        Origin: 'https://bummy-suri.com',
-      },
-    })
-      .then(res => {
-        return res.blob();
-      })
-      .then(blob => {
-        saveAs(blob, 'myNFT');
-        // setOpen(false);
-      })
-      .catch(err => {
-        console.error('error : ', err);
-      });
+  // const downloadNFT = () => {
+  //   const url = 'https://nftmetadata2022.s3.ap-northeast-2.amazonaws.com/images/1.png';
+  //   fetch(url, {
+  //     mode: 'no-cors',
+  //     method: 'GET',
+  //     headers: {
+  //       Origin: 'https://bummy-suri.com',
+  //     },
+  //   })
+  //     .then(res => {
+  //       return res.blob();
+  //     })
+  //     .then(blob => {
+  //       saveAs(blob, 'myNFT');
+  //       // setOpen(false);
+  //     })
+  //     .catch(err => {
+  //       console.error('error : ', err);
+  //     });
+  // };
+
+  const cardRef = useRef<HTMLImageElement>(null);
+  const onDownloadBtn = () => {
+    const card = cardRef?.current;
+    if (!card) {
+      return;
+    }
+    domtoimage.toBlob(card).then(blob => {
+      saveAs(blob, 'card.png');
+    });
   };
 
   return (
     <LogoDIv height={window.innerHeight}>
-      <img src={moving} style={{ width: '240px', height: '230px' }} />
-      <img style={{ width: '200px' }} src="https://nftmetadata2022.s3.ap-northeast-2.amazonaws.com/images/1.png"></img>
-      <button onClick={() => downloadNFT()}>이미지 다운로드 받기</button>/
+      {/* <img src={moving} style={{ width: '240px', height: '230px' }} /> */}
+      <img
+        className="card"
+        ref={cardRef}
+        style={{ width: '200px' }}
+        src="https://nftmetadata2022.s3.ap-northeast-2.amazonaws.com/images/1.png"></img>
+      <button onClick={() => onDownloadBtn()}>이미지 다운로드 받기</button>/
     </LogoDIv>
   );
 }
